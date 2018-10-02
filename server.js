fc2628a262797f36eba1e72edce7d09a32d03365
Anthony Curtis <tonydc1997@gmail.com>
2018-10-02 06:33:25 -0700

    Create a POST request

--- server.js
+++ server.js
@@ -7,3 +7,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req,res) => res.send('Hello World!'));
+
+app.post('/secret', (req, res) => {
+  const { userInput } = req.body;
+  console.log(req.body);
+});
