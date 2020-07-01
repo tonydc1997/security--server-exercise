03e57cc5e38ccda29ef69fddd44553a6df875402
Anthony Curtis <tonydc1997@gmail.com>
2020-07-01 11:06:19 -0400

    chore: Auto-format code with Prettier

--- server.js
+++ server.js
@@ -1,51 +1,50 @@
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const winston = require('winston');
-const helmet = require('helmet');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const winston = require("winston");
+const helmet = require("helmet");
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const app = express();
 
-var whitelist = ['https://tonydc1997.github.io/']
+var whitelist = ["https://tonydc1997.github.io/"];
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error("Not allowed by CORS"));
     }
-  }
-}
+  },
+};
 
 app.use(helmet());
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
-app.get('/', (req,res) => {
-  res.cookie('session', '1', {
-    httpOnly: true, 
+app.get("/", (req, res) => {
+  res.cookie("session", "1", {
+    httpOnly: true,
     secure: true,
-    sameSite: true
-    });
+    sameSite: true,
+  });
   res.set({
-    'Content-Security-Policy': "'script-src', 'self' 'https://apis.google.com'"
-  })
-  res.send('Hello World!')
+    "Content-Security-Policy": "'script-src', 'self' 'https://apis.google.com'",
+  });
+  res.send("Hello World!");
 });
 
-
-app.post('/secret', (req, res) => {
+app.post("/secret", (req, res) => {
   const { userInput } = req.body;
   winston.log(userInput);
   if (userInput) {
-    winston.log('info', 'user input: ' + userInput);
-    res.status(200).json('success');
+    winston.log("info", "user input: " + userInput);
+    res.status(200).json("success");
   } else {
-    winston.error('Something may have gone wrong: ' + userInput);
-    res.status(400).json('incorrect submission');
+    winston.error("Something may have gone wrong: " + userInput);
+    res.status(400).json("incorrect submission");
   }
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000'));
+app.listen(3000, () => console.log("Example app listening on port 3000"));
